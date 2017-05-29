a3c58ff04d56972f4782712d10067edde44dd410
Wim <liwangmj@gmail.com>
2017-05-29 21:04:20 +0800

    修改manjaro

--- ops/manjaro-i3.sh
+++ ops/manjaro-i3.sh
@@ -8,7 +8,7 @@ k_username=wim
 # 安装基础应用
 pacman -Syyu
 # yaourt -S base-devel perl lua51 python2 vim gimp fcitx unzip unrar curl epdfview
-yaourt -S docker docker-compose cmake git svn valgrind gdb luajit python3 python-pip python2-pip adobe-source-code-pro-fonts wqy-microhei xmind eclipse-jee atom qtcreator kdesvn mysql-workbench mysql-utilities freerdp remmina gnu-netcat wireshark-qt nload tcpdump chromium firefox filezilla dia vlc thunderbird fcitx-configtool kcm-fcitx fcitx-im lrzsz tmux leafpad calc wine winetricks create_ap create_ap-gui meld doxygen graphviz shadowsocks-qt5 shadowsocks dos2unix zip unrar wget
+yaourt -S docker docker-compose cmake git subversion valgrind gdb luajit python3 python-pip python2-pip adobe-source-code-pro-fonts wqy-microhei xmind eclipse-jee atom qtcreator kdesvn mysql-workbench mysql-utilities freerdp remmina gnu-netcat wireshark-qt lsof nload tcpdump chromium firefox filezilla dia vlc thunderbird fcitx-configtool kcm-fcitx fcitx-im lrzsz tmux leafpad calc wine winetricks create_ap create_ap-gui meld doxygen graphviz shadowsocks-qt5 shadowsocks dos2unix zip unrar wget
 
 # 安装扩展应用
 # yaourt -S fcitx-sogoupinyin gitkraken android-studio dbeaver-ee packetsender soapui wps-office pencil electronic-wechat nutstore teamviewer youdao-dict shutter
