f2fdb14909dab9d75573a849b8ec908310aed0b6
Wim <liwangmj@gmail.com>
2017-06-07 20:58:18 +0800

    修改manjaro

--- ops/manjaro-i3.sh
+++ ops/manjaro-i3.sh
@@ -8,10 +8,10 @@ k_username=wim
 # 安装基础应用
 pacman -Syyu
 # yaourt -S base-devel perl lua51 python2 vim gimp fcitx unzip unrar curl epdfview
-yaourt -S docker docker-compose cmake git subversion valgrind gdb luajit python3 python-pip python2-pip adobe-source-code-pro-fonts wqy-microhei xmind eclipse-jee atom qtcreator kdesvn mysql-workbench mysql-utilities freerdp remmina gnu-netcat wireshark-qt lsof nload tcpdump chromium firefox filezilla dia vlc thunderbird fcitx-configtool kcm-fcitx fcitx-im lrzsz tmux leafpad calc wine winetricks create_ap create_ap-gui meld doxygen graphviz shadowsocks-qt5 shadowsocks dos2unix zip unrar wget
+yaourt -S docker docker-compose cmake git subversion valgrind gdb luajit python3 python-pip python2-pip adobe-source-code-pro-fonts wqy-microhei xmind eclipse-jee atom qtcreator mysql-workbench mysql-utilities freerdp remmina gnu-netcat wireshark-qt lsof nload tcpdump chromium firefox filezilla dia vlc thunderbird fcitx-configtool kcm-fcitx fcitx-im lrzsz tmux leafpad calc wine winetricks create_ap create_ap-gui meld doxygen graphviz shadowsocks-qt5 shadowsocks dos2unix zip unrar wget
 
 # 安装扩展应用
-# yaourt -S fcitx-sogoupinyin gitkraken android-studio dbeaver-ee packetsender soapui wps-office pencil electronic-wechat nutstore teamviewer youdao-dict shutter
+# yaourt -S fcitx-sogoupinyin gitkraken kdesvn pepper-flash android-studio dbeaver-ee packetsender soapui wps-office pencil electronic-wechat gitter nutstore teamviewer youdao-dict shutter
 
 pip2 install --upgrade setuptools pip
 pip install --upgrade backports.ssl_match_hostname
@@ -21,14 +21,6 @@ systemctl start docker.service
 gpasswd -a ${k_username} docker
 
 winetricks mfc42
-
-systemctl enable teamviewerd
-systemctl start teamviewerd
-
-cd /tmp
-wget -c https://fpdownload.adobe.com/get/flashplayer/pdc/24.0.0.221/flash_player_npapi_linux.x86_64.tar.gz
-tar -xzf flash_player_npapi_linux.x86_64.tar.gz
-mv libflashplayer.so /usr/lib/chromuium/
 
 # 关闭机箱喇叭
 echo 'blacklist pcspkr' > /etc/modprobe.d/nopcspkr.conf
